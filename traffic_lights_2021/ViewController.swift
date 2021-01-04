ac53d2be26271ddcf5760b4d39ef4a5f8edced8e
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-01-04 11:12:11 +0300

    Initial Commit

---
+++ traffic_lights_2021/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traffic_lights_2021
+//
+//  Created by Vladimir Syleimanov on 04.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
