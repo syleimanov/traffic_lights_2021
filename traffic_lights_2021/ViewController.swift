4ea48a9eba09e1034cf0f6ca63c5b01b307b6f7f
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-01-19 20:59:19 +0300

    первый коммит main

--- traffic_lights_2021/ViewController.swift
+++ traffic_lights_2021/ViewController.swift
@@ -30,61 +30,12 @@ class ViewController: UIViewController {
         yellowLight.alpha = lightIsOff
         greenLight.alpha = lightIsOff
         
-//        redLight.layer.cornerRadius = redLight.frame.size.width / 2
-//        yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
-//        greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
-//
         startButton.layer.cornerRadius = 15
         redLight.layer.cornerRadius = 65
         greenLight.layer.cornerRadius = 65
         yellowLight.layer.cornerRadius = 65
      
-
     }
-    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        redLight.layer.cornerRadius = redLight.frame.size.width / 2
-//        yellowLight.layer.cornerRadius = yellowLight.frame.size.width / 2
-//        greenLight.layer.cornerRadius = greenLight.frame.size.width / 2
-//
-//        startButton.layer.cornerRadius = 15
-//
-//        redLight.alpha = 0.3
-//        yellowLight.alpha = 0.3
-//        greenLight.alpha = 0.3
-//
-//    }
-
-//    @IBAction func startButtonPressed() {
-//        if startButton.isEnabled {
-//            redLight.alpha = 1
-//            startButton.setTitle("Next", for: .normal)
-//        } else if redLight.alpha == 1 {
-//            redLight.alpha = 0.3
-//            yellowLight.alpha = 1
-//        } else if yellowLight.alpha == 1 {
-//            yellowLight.alpha = 0.3
-//            greenLight.alpha = 1
-//        }  else if greenLight.alpha == 1 {
-//            greenLight.alpha = 0.3
-//            redLight.alpha = 1
-//        }
-//    
-    
-//        if startButton.titleLabel?.text == "START" {
-//            redLight.alpha = 1
-//            startButton.setTitle("NEXT", for: .normal)
-//        } else if redLight.alpha == 1 {
-//            redLight.alpha = 0.3
-//            yellowLight.alpha = 1
-//        } else if yellowLight.alpha == 1 {
-//            yellowLight.alpha = 0.3
-//            greenLight.alpha = 1
-//        }  else if greenLight.alpha == 1 {
-//            greenLight.alpha = 0.3
-//            redLight.alpha = 1
-//        }
 
  
     @IBAction func buttonPressed() {
